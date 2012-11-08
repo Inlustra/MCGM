028934d65af612de2f587e734db70256ecc5bece
Patt-tom McDonnell <spero78@spero78.com>
2012-11-08 02:10:28 +0000

    Trembling Blocks

---
+++ minecraftGameModesMinigames/src/TremblingBlocks.java
@@ -0,0 +1,56 @@
+
+import com.mcgm.Plugin;
+import com.mcgm.game.Minigame;
+import com.mcgm.game.provider.GameInfo;
+import com.mcgm.utils.Misc;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+
+
+@GameInfo(name = "Trembling Blocks", aliases = {"TB"}, pvp = true, authors = {"Pt"},
+gameTime = 125, description = "Stand still and shoot, dont turn back")
+public class TremblingBlocks extends Minigame {
+    
+    Location area = new Location(Misc.mainWorld(), Misc.mainWorld().getSpawnLocation().getBlockX(), Misc.mainWorld().getSpawnLocation().getBlockY()+100, Misc.mainWorld().getSpawnLocation().getBlockZ());
+
+    public TremblingBlocks(Plugin p, Player... playing) {
+        super(p, TremblingBlocks.class.getAnnotation(GameInfo.class), playing);
+    }
+
+    @Override
+    public void minigameTick() {
+    }
+
+    @Override
+    public void onCountDown() {
+    }
+
+    @Override
+    public void onTimeUp() {
+    }
+
+    @Override
+    public void startGame() {
+        for(int i=0; i<100; i++){
+            Location spawn = new Location(area.getWorld(),area.getBlockX()+Misc.getRandom(-5, 5),area.getBlockY(),area.getBlockZ()+Misc.getRandom(-5, 5));
+            spawn.getBlock().setType(Material.BAKED_POTATO);
+        }
+        for(Player p : playing){
+            p.teleport(area);
+        }
+    }
+
+    @Override
+    public void onEnd() {
+    }
+
+    @Override
+    public void generateGame() {
+    }
+
+    @Override
+    public void onLeaveArea() {
+    }
+}
