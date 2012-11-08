09dc2bbb246e90c9d83508b612bdf308073b2dd0
Thomas Nairn <tjnairn@gmail.com>
2012-11-08 01:13:14 +0000

    Added default Minigame

---
+++ minecraftGameModesMinigames/src/DefaultMinigame.java
@@ -0,0 +1,53 @@
+
+import com.mcgm.Plugin;
+import com.mcgm.game.Minigame;
+import com.mcgm.game.provider.GameInfo;
+import com.mcgm.utils.Misc;
+import com.sk89q.worldedit.Vector;
+import java.util.ArrayList;
+import org.bukkit.entity.Player;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author Thomas
+ */
+@GameInfo(name = "Default Minigame", aliases = {"DM"}, pvp = false, authors = {"Tom"},
+gameTime = 65, description = "desc")
+public class DefaultMinigame extends Minigame {
+
+    public DefaultMinigame(Plugin p, Player... playing) {
+        super(p, DefaultMinigame.class.getAnnotation(GameInfo.class), playing);
+    }
+
+    @Override
+    public void minigameTick() {
+    }
+
+    @Override
+    public void onCountDown() {
+    }
+
+    @Override
+    public void onTimeUp() {
+    }
+
+    @Override
+    public void startGame() {
+    }
+
+    @Override
+    public void onEnd() {
+    }
+
+    @Override
+    public void generateGame() {
+    }
+
+    @Override
+    public void onLeaveArea() {
+    }
+}
